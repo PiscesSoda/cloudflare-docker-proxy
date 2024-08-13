1dedce96e8f32262908dafa21a878371d2221e5d
PiscesSoda <61488102+PiscesSoda@users.noreply.github.com>
2024-08-14 00:39:19 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.life-meaningless.icu": dockerHub,
+  "quay.life-meaningless.icu": "https://quay.io",
+  "gcr.life-meaningless.icu": "https://gcr.io",
+  "k8s-gcr.life-meaningless.icu": "https://k8s.gcr.io",
+  "k8s.life-meaningless.icu": "https://registry.k8s.io",
+  "ghcr.life-meaningless.icu": "https://ghcr.io",
+  "cloudsmith.life-meaningless.icu": "https://docker.cloudsmith.io",
+  "ecr.life-meaningless.icu": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.life-meaningless.icu": dockerHub,
 };
 
 function routeByHosts(host) {
